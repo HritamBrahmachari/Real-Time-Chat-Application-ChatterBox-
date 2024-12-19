dc1fb7d29c05f8cfcd4045634ccc6cf55a3a4ae6
Hritam Brahmachari <hritambrahmachari1@gmail.com>
2024-12-19 05:35:31 -0800

    Resolved merge conflicts

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json()); 
 app.use(cookieParser());
 const corsOption={
-    origin:'https://frolicking-lokum-d325fc.netlify.app',
+    origin:'https://illustrious-lebkuchen-30a599.netlify.app/login',
     credentials:true
 };
 app.use(cors(corsOption)); 
