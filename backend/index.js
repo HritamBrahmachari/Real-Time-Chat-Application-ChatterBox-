b5db6eb2a4fc443a15026e57e319c7e0dcefc8ca
Hritam Brahmachari <hritambrahmachari1@gmail.com>
2024-12-19 05:28:52 -0800

    Resolved merge conflicts

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json()); 
 app.use(cookieParser());
 const corsOption={
-    origin:'https://stirring-starship-e41ab4.netlify.app/',
+    origin:'https://frolicking-lokum-d325fc.netlify.app',
     credentials:true
 };
 app.use(cors(corsOption)); 
