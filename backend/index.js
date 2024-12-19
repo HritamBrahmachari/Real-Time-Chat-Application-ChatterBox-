f80fc4918b740668fe75b6e4e4646359a09a7c07
Hritam Brahmachari <hritambrahmachari1@gmail.com>
2024-12-19 05:02:50 -0800

    Resolved merge conflicts

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json()); 
 app.use(cookieParser());
 const corsOption={
-    origin:'https://messaging-app-1-4gsh.onrender.com/',
+    origin:'https://stirring-starship-e41ab4.netlify.app/',
     credentials:true
 };
 app.use(cors(corsOption)); 
